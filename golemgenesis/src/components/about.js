87637b17349386baa060490ca2fbbe385d965829
N.M. Porterfield <ccmstudiosinc@gmail.com>
2021-03-29 21:32:56 -0400

    media queries

--- golemgenesis/src/components/about.js
+++ golemgenesis/src/components/about.js
@@ -5,19 +5,8 @@ import ChrisCutter from '../assets/images/ChrisCutter.png'
 class About extends React.Component{
     render(){
         return(
-            // <div id="featured" className="card border-dark bg-dark text-white mb-12 content-justify-center">
-            //     <img src={Golemgray} alt="featured Book" className="card-img-top" alt="..." />
 
-            //     <div className="card-body bg-light">
-            //         <h1 className="card-title">Golem Genesis</h1>
-            //         <h4 className="card-text">In the world of Terra, Humans and Golems live side by side. Golems are living monoliths, beings that come from an ancient race of warriors and protectors.  
-            //         Golems still battle today in the G-Arena where Golems and their Guardians battle each other for the title of Gladiator Supreme. Chris Carter, the son of a world renowned Golem specialist gone missing, finds a Golem buried in a cave on his way home from school. After helping him out of the cave and protecting him in his first fight, 
-            //         the Golem takes to him and Chris finds, not only a new friend but a new adventure to embark on full of lots of awesome battles and new Golems. Let's Rock!</h4>
-            //         <a href="https://urfavoriteauthor.square.site/product/golem-gladiator-s-rock-issue-1/14?cs=true" target='_blank' className='btn btn-outline-dark'>Purchase here!</a>
-            //         <h6 className="card-text"><small className="text-muted">RZN8 Print and Publishing</small></h6>
-            //     </div>
-            // </div>
-            <div id="featured" className="card border-dark bg-dark text-white mb-4 content-justify-center" style={{width:1000}}>
+            <div id="featured" className="card border-dark bg-dark text-white mb-4 content-justify-center">
             <div className="card bg-dark">
   <img className="card-img" id='aboutimg'src={ChrisCutter} alt="Card image"/>
   <div className="card-img-overlay">
@@ -25,7 +14,7 @@ class About extends React.Component{
     <br></br>
     <br></br>
     <br></br>
-    <h4 className="card-text" id='abouttext'style={{fontSize:45}}>In the world of Terra, Humans and Golems live side by side. Golems are living monoliths, beings that come from an ancient race of warriors and protectors.         
+    <h4 className="card-text" id='abouttext'>In the world of Terra, Humans and Golems live side by side. Golems are living monoliths, beings that come from an ancient race of warriors and protectors.         
     Golems still battle today in the G-Arena where Golems and their Guardians battle each other for the title of Gladiator Supreme. Chris Carter, the son of a world renowned Golem specialist gone missing, finds a Golem buried in a cave on his way home from school. After helping him out of the cave and protecting him in his first fight, 
     the Golem takes to him and Chris finds, not only a new friend but a new adventure to embark on full of lots of awesome battles and new Golems. Let's Rock!</h4>
   </div>
